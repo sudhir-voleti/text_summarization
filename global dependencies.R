47e66a1be55a8941e8fc8b4cb9903976b50967d6
Sudhir Voleti <sudhir_voleti@isb.edu>
2019-05-31 18:14:38 +0530

    Add files via upload

---
+++ global dependencies.R
@@ -0,0 +1,9 @@
+if (!require(shiny)) {install.packages("shiny")}
+if (!require(dplyr)) {install.packages("dplyr")}
+if (!require(tidytext)) {install.packages("tidytext")}
+if (!require(textrank)) {install.packages("textrank")}
+
+library(shiny)
+library(dplyr)
+library(tidytext)
+library(textrank)
